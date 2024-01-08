52b3247cfbedd546c5ba02154bbcd52cf7463128
Nishank Phulera <nishank.phulera@genuinemark.org>
2024-01-08 12:51:54 +0530

    changes

--- android/app/src/main/java/com/ultimatrue/MainActivity.java
+++ android/app/src/main/java/com/ultimatrue/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Ultimatrue";
+    return "ultimatrue";
   }
   @Override
   protected void onCreate(Bundle savedInstanceState) {
